9d0241c016b85d7a62c96865d3e4442e641163f5
Boss, Brendan <Brendan.Boss@allscripts.com>
2018-07-25 13:45:24 -0400

    Added Libraries

--- Minesweeper++/Minesweeper++/Main.cpp
+++ Minesweeper++/Minesweeper++/Main.cpp
@@ -1,5 +1,33 @@
 #include "SDL.h"
+#include <stdio.h>
+#include <iostream>
+
+bool init();
+
+bool loadMedia();
+
+void close();
+
+SDL_Window* gWindow = NULL;
+
+SDL_Surface* gScreenSurface = NULL;
+
+SDL_Surface* gHelloWorld = NULL;
+
+bool init() {
+	bool success = true;
+
+	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+		std::cout << "SDL could not initialize! SDL_Error: " << SDL_GetError() << std::endl;
+		success = false;
+	}
+	else {
+
+	}
+}
 
 int main(int argc, char* argv[]) {
+
+
 	return 0;
 }
